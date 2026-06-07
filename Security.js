260a440c8bb71e0421fd4e86fed9c7f516a9f3f3
ruikey285-collab <ruikey285@gmail.com>
2026-06-08 04:41:16 +0530

    Create Security.js

---
+++ Security.js
@@ -0,0 +1,8 @@
+function applySecurity(app) {
+    // बेसिक सुरक्षा मिडिलवेयर
+    console.log("[SECURITY] Security protocols activated.");
+    
+    // आप यहां सुरक्षा से जुड़े और भी नियम जोड़ सकते हैं
+}
+
+module.exports = applySecurity;
