c363ea4c2172107b905dad3762dcda939da68d31
ruikey285-collab <ruikey285@gmail.com>
2026-06-09 07:34:14 +0530

    Update PaymentGateway.js

--- PaymentGateway.js
+++ PaymentGateway.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const QRCode = require('qrcode');
 
+router.post('/add-money', async (req, res) => {
+    try {
+        const { amount } = req.body;
+        const realUpilId = "9302431003@hdfcbank";
+        
+        if (!amount || amount < 10) {
+            return res.status(400).json({ error: "Min 10" });
+        }
+
+        const url = await QRCode.toDataURL(`upi://pay?pa=${realUpilId}&pn=Raju&am=${amount}&cu=INR`);
+        res.json({ qrCode: url });
+    } catch (e) {
+        res.status(500).json({ error: "Error" });
+    }
+});
+
+module.exports = router;
